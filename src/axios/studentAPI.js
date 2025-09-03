26539f4bdff62d0491921b63261a675fb749f99f
rakeshnit322 <rakeshnit322@gmail.com>
2025-09-03 23:56:19 +0530

    Add Netlify configuration and update API base URL to production

--- src/axios/studentAPI.js
+++ src/axios/studentAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000/api/students',
+    baseURL: 'https://api-sgms-backend-new.onrender.com/api/students',
     headers: {
         'Content-Type': 'application/json'
     }
